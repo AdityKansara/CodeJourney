222b6b431205886a504d3cde4d60383144b90c4c
Adity <adity.kansara36@gmail.com>
2026-01-14 23:50:17 +0530

    Remove Duplicates from Array Added!

---
+++ Easy/26.py
@@ -0,0 +1,14 @@
+# #26 - Remove Duplicates from Sorted Array
+# Problem solved by Adity
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        j = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[j] = nums[i]
+                j += 1
+        return j
